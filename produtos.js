bc4d02209cd47b6a6f658d5a65ef9c9c437e867a
Felipe Alves <felipealves3580@gmail.com>
2023-01-20 11:39:51 -0300

    Adiciona lista de produtos

---
+++ produtos.js
@@ -0,0 +1,28 @@
+const productsList = [
+  {
+    capaProduto: "./img/naruto27.webp",
+    nome: "Naruto Vol. 27",
+    descricao: "Capa naruto vol 27",
+    preco: 22.9,
+  },
+
+  {
+    capaProduto: "./img/bleach.webp",
+    nome: "Bleach Vol.9",
+    descricao: "Capa bleach vol 9",
+    preco: 22.9,
+  },
+
+  {
+    capaProduto: "./img/fire-force.webp",
+    nome: "Fire Force Vol.27",
+    descricao: "Capa fire force vol 27",
+    preco: 22.9,
+  },
+  {
+    capaProduto: "./img/one-piece.webp",
+    nome: "One Piece Vol.4",
+    descricao: "Capa one piece vol 4",
+    preco: 22.9,
+  },
+];
